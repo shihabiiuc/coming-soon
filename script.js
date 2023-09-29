c37387e96f4ae9c3446ebfe08c4ed7703e333eb5
Shihab <hello@shihabiiuc.com>
2023-09-29 19:02:58 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Set the launch date for your website (YYYY, MM-1, DD, HH, MM, SS)
+const launchDate = new Date(2023, 9, 30, 12, 0, 0);
+
+function updateCountdown() {
+    const currentDate = new Date();
+    const timeDifference = launchDate - currentDate;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days.toString().padStart(2, '0');
+    document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
