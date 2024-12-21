0dca067f744b336b2526b7456620a5f4bfbc1183
suh <shihabdinajpur@gmail.com>
2024-12-21 21:08:53 +0600

    Updated launch date

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 // Set the launch date for your website (YYYY, MM-1, DD, HH, MM, SS)
-const launchDate = new Date(2024, 5, 19, 12, 0, 0);
+const launchDate = new Date(2025, 3, 0, 12, 0, 0);
 
 function updateCountdown() {
-    const currentDate = new Date();
-    const timeDifference = launchDate - currentDate;
+  const currentDate = new Date();
+  const timeDifference = launchDate - currentDate;
 
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    document.getElementById("days").textContent = days.toString().padStart(2, '0');
-    document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
-    document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
-    document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
+  document.getElementById("days").textContent = days
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("hours").textContent = hours
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("minutes").textContent = minutes
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("seconds").textContent = seconds
+    .toString()
+    .padStart(2, "0");
 }
 
 setInterval(updateCountdown, 1000);
